326e02178f8af68f2eb7b73c3fef95cdaa24bb21
David Brokaw <dgbrokaw2@gmail.com>
2016-12-05 13:05:56 -0500

    3 variations of executing simple code from a persistent/non-persistent background script that modifies the DOM

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+	// All three variations do the same thing.
 	chrome.tabs.executeScript({code: "var div = document.createElement('div'); div.innerHTML = 'hiii'; document.body.appendChild(div);"});
 	// chrome.tabs.executeScript(null, {code: "var div = document.createElement('div'); div.innerHTML = 'hiii'; document.body.appendChild(div);"});
 	// chrome.tabs.executeScript(tab.id, {code: "var div = document.createElement('div'); div.innerHTML = 'hiii'; document.body.appendChild(div);"});
