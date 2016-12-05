3f0d9ef66cc0d064937a9e4a82c2256681e2b89c
David Brokaw <dgbrokaw2@gmail.com>
2016-12-05 16:09:53 -0500

    cannot use tabs API within a content script

--- background.js
+++
@@ -1,3 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(tab.id, {code: "var div = document.createElement('div'); div.innerHTML = 'hiii'; document.body.appendChild(div);"});
-})
---
+++ script.js
@@ -0,0 +1,4 @@
+console.log(document);
+var div = document.createElement('div');
+div.innerHTML = 'hiii';
+document.body.appendChild(div);
