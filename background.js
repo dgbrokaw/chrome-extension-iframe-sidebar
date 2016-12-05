0577f0b91d1ab950e76f3605d4afd664face198a
David Brokaw <dgbrokaw2@gmail.com>
2016-12-05 17:31:06 -0500

    executing libraries with executeScript

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript({file: "libs/d3/d3.min.js"});
+	chrome.tabs.executeScript({file: "libs/jquery/jquery-3.1.0.min.js"});
+	chrome.tabs.executeScript({file: "libs/bootstrap/js/bootstrap.min.js"});
 	chrome.tabs.executeScript({file: "libs/gmath/gmath.min.js"});
 });
