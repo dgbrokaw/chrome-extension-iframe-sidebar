1dcaf6190923154737464839fab92001ba5dcb41
David Brokaw <dgbrokaw2@gmail.com>
2016-12-05 16:22:45 -0500

    does message passing work from anonymous script

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-	chrome.tabs.executeScript(tab.id, {code: "var div = document.createElement('div'); div.innerHTML = 'hiii'; document.body.appendChild(div);"});
-})
+	chrome.tabs.executeScript({file: "script.js"});
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  console.log(sender.tab ?
+              "from a content script:" + sender.tab.url :
+              "from the extension");
+  if (request.greeting == "hello")
+    sendResponse({farewell: "goodbye"});
+});
